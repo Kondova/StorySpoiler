7040a04e236c583ea7a299f75ea6b3d53ba93a6b
Pavleta Kondova <pavletakondova@gmail.com>
2025-08-16 12:20:43 +0300

    Add StorySpoiler files

---
+++ StorySpoler/UnitTest1.cs
@@ -0,0 +1,163 @@
+﻿using System.Text.Json;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Net;
+using StorySpoler.Models;
+
+
+namespace StorySpoler
+{
+    [TestFixture]
+    public class StoryTests
+    {
+        private RestClient client;
+        private static string createdStoryId;
+        private static string baseURL = "https://d3s5nxhwblsjbi.cloudfront.net";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            // Взимаме токен за достъп
+            string token = GetJwtToken("pavleta123", "123123");
+
+            // Създаваме клиент с токен
+            var options = new RestClientOptions(baseURL)
+            {
+                Authenticator = new JwtAuthenticator(token)
+            };
+
+            client = new RestClient(options);
+        }
+
+        // Метод за логин и получаване на токен
+        private string GetJwtToken(string username, string password)
+        {
+            var loginClient = new RestClient(baseURL);
+
+            var request = new RestRequest("/api/User/Authentication", Method.Post);
+            request.AddJsonBody(new { username, password });
+
+            var response = loginClient.Execute(request);
+
+            var json = JsonSerializer.Deserialize<JsonElement>(response.Content);
+            return json.GetProperty("accessToken").GetString();
+        }
+
+        [Test, Order(1)]
+        public void CreateStory_ShouldReturnCreated()
+        {
+            var story = new
+            {
+                Title = "New Story",
+                Description = "Test story description",
+                Url = ""
+            };
+
+            var request = new RestRequest("/api/Story/Create", Method.Post);
+            request.AddJsonBody(story);
+
+            var response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Created));
+
+            var json = JsonSerializer.Deserialize<JsonElement>(response.Content);
+            createdStoryId = json.GetProperty("storyId").GetString();
+            Assert.That(response.Content, Does.Contain("Successfully created!"));
+        }
+
+        [Test, Order(2)]
+        public void GetAllStories_ShouldReturnList()
+        {
+            var request = new RestRequest("/api/Story/All", Method.Get);
+            var response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var stories = JsonSerializer.Deserialize<List<object>>(response.Content);
+            Assert.That(stories, Is.Not.Empty);
+        }
+
+        [Test, Order(3)]
+        public void EditStory_ShouldReturnOk()
+        {
+            var changes = new StoryDTO
+            {
+                Title = "new title", 
+                Description = "new description", 
+                Url = "" 
+            };
+
+            var request = new RestRequest($"/api/Story/Edit/{createdStoryId}", Method.Put);
+            request.AddJsonBody(changes);
+
+            var response = client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content, Does.Contain("Successfully edited"));
+        }
+
+        [Test, Order(4)]
+        public void DeleteStory_ShouldReturnOk()
+        {
+            var request = new RestRequest($"/api/Story/Delete/{createdStoryId}", Method.Delete);
+            var response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content, Does.Contain("Deleted successfully!"));
+        }
+
+        [Test, Order(5)]
+        public void CreateStory_WithoutRequiredFields_ShouldReturnBadRequest()
+        {
+            var story = new
+            {
+                Title = "",
+                Description = ""
+            };
+
+            var request = new RestRequest("/api/Story/Create", Method.Post);
+            request.AddJsonBody(story);
+
+            var response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        }
+
+        [Test, Order(6)]
+        public void EditNonExistingStory_ShouldReturnNotFound()
+        {
+            string fakeId = "123";
+            var changes = new StoryDTO
+            {
+                Title = "new title",
+                Description = "new description",
+                Url = ""
+            };
+
+            var request = new RestRequest($"/api/Story/Edit/{fakeId}", Method.Put);
+            request.AddJsonBody(changes);
+
+            var response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            Assert.That(response.Content, Does.Contain("No spoilers..."));
+        }
+
+        [Test, Order(7)]
+        public void DeleteNonExistingStory_ShouldReturnBadRequest()
+        {
+            string fakeId = "123";
+            var request = new RestRequest($"/api/Story/Delete/{fakeId}", Method.Delete);
+            var response = client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            Assert.That(response.Content, Does.Contain("Unable to delete this story spoiler!"));
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            client?.Dispose();
+        }
+    }
+}
